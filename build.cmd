9b804a4be350ddb55350e44f60e642cc6aac26c6
ayuto@web.de <ayuto@web.de>
2014-10-30 13:23:23 +0000

    - symdump is now ready to be build for Python 3

--- build.cmd
+++ build.cmd
@@ -1,4 +1,7 @@
-@path %path%;C:\MinGW\bin
+@path C:\Python34;%path%;C:\MinGW\bin
+set PYTHONHOME=C:\Python34
+set PATH=%PATH%;C:\Python34
+set LIB=%LIB%;C:\Python34\Lib
 python setup.py build_ext -c mingw32 --inplace
 @echo =================================================================
 @pause
